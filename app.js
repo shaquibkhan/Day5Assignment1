356a9c7eddd52d25e52b28738c890f70958c0172
shaquibkhan <connectshaquib@gmail.com>
2017-04-26 12:31:31 +0530

    commit
    commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//variables declared for storing info..like string,integer etc..
+var name="SHAQUIB AHMAD KHAN";
+var age=23;
+var dob=("29-12-1993");
+var pob="NEW-DELHI";
+// to get the result into the browser console..
+console.log("NAME IS :" +name);
+console.log("AGE IS :" +age);
+console.log("DATE OF BIRTH IS :" +dob);
+console.log("PLACE OF BIRTH IS :" +pob);
